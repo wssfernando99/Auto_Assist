87bff8c0ec998b7191157baeabff759f566b17c0
Sayuranga Fernando <sarindusauranga@gmail.com>
2025-02-03 14:46:56 +0530

    fic: script

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -41,6 +41,8 @@
     <link rel="stylesheet" href="{{ asset('assets/css/core.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/theme-default.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/nav.css') }}">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
     
 
@@ -60,7 +62,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
 
     <script src="{{ asset('assets/js/helpers.js') }}"></script>
     <script src="{{ asset('assets/js/main.js') }}"></script>
