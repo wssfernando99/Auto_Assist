7fdce6a26fe3785d313b79d164f2d12ebf03c052
Sayuranga Fernando <sarindusauranga@gmail.com>
2025-02-04 22:21:34 +0530

    feat: update layout

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -43,6 +43,15 @@
     <link rel="stylesheet" href="{{ asset('assets/css/nav.css') }}">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+
+            <script>
+                $(document).ready(function() {
+                    $('#myTable').DataTable();
+                });
+            </script>
     
     
 
