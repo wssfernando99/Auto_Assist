d9c8a05577c78c3ca73ed2a7cd16b391fc5a7531
Sayuranga Fernando <sarindusauranga@gmail.com>
2025-02-22 16:37:05 +0530

    feat: service tables

---
+++ database/migrations/2025_02_21_080954_create_service_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('serviceId');
+            $table->string('inspection')->nullable();
+            $table->string('Deficiencies')->nullable();
+            $table->string('service')->nullable();
+            $table->double('checkId')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_details');
+    }
+};
